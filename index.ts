1e11755de7a637b64ff40ed03576598d23e89997
AymenFarhani <36412442+AymenFarhani@users.noreply.github.com>
2025-02-20 09:35:26 +0100

    Add files via upload

--- index.ts
+++ index.ts
@@ -14,10 +14,10 @@ let nextId : number  = 1;
   const project3: Project = {id:nextId++, title:"Project C", cost:1250, startDate:new Date("2020-12-06")};
   projectManager.createProject(project3);
   console.log(projectManager.getAllProjects());
-  console.log("====================================");
+  console.log("-----------------------------------");
   projectManager.updateProject(project1.id, { cost: 1200 });
   console.log(projectManager.getProject(project1.id));
-  console.log("====================================");
+  console.log("-----------------------------------");
   projectManager.deleteProject(project3.id);
   console.log(projectManager.getAllProjects());
 
@@ -28,6 +28,10 @@ const studentManager = new StudentManager();
 studentManager.createStudent({ id: 1, fullName: "John Doe", email: "john@example.com" });
 studentManager.createStudent({ id: 2, fullName: "Jane Smith", email: "jane@example.com" });
 
+console.log("-----------------------------------");
 studentManager.getStudent(1); // Retrieve the student with id 1
+
+console.log("-----------------------------------");
 studentManager.updateStudent(1, { fullName: "Johnathan Doe" }); // Update student with id 1
+console.log("-----------------------------------");
 studentManager.deleteStudent(2); 
